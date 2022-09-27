a7b82ee834a714de2028cdf9776250c3ebe9564e
ct-omprakash <98209815+ct-omprakash@users.noreply.github.com>
2022-09-27 12:53:55 +0530

    package upgrades - npm (#10)
    * package upgrades - npm
    
    * package upgrades - yarn
    
    * changed version
    
    Co-authored-by: Harshad <codedthemes@gmail.com>

--- src/components/Loadable.js
+++ src/components/Loadable.js
@@ -5,10 +5,11 @@ import Loader from './Loader';
 
 // ==============================|| LOADABLE - LAZY LOADING ||============================== //
 
-const Loadable = (Component) => (props) => (
-    <Suspense fallback={<Loader />}>
-        <Component {...props} />
-    </Suspense>
-);
+const Loadable = (Component) => (props) =>
+    (
+        <Suspense fallback={<Loader />}>
+            <Component {...props} />
+        </Suspense>
+    );
 
 export default Loadable;
