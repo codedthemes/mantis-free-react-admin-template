30f54e1e7466c3ff1271825861f7a47c2dc46ef1
Nate Schultz <nate.schultz@outlook.com>
2022-12-08 21:29:14 -0500

    added favicon

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,5 +1,6 @@
 // material-ui
 import { useTheme } from '@mui/material/styles';
+import FingerprintIcon from '@mui/icons-material/Fingerprint';
 
 // import logoDark from 'assets/images/logo-dark.svg';
 // import logo from 'assets/images/logo.svg';
@@ -7,7 +8,12 @@ import { useTheme } from '@mui/material/styles';
 // ==============================|| LOGO SVG ||============================== //
 
 const Logo = () => {
-    return <></>;
+    return (
+        <>
+            <FingerprintIcon></FingerprintIcon>
+            Nano Swap
+        </>
+    );
 };
 
 export default Logo;
