da440fb031d2e9cfbe53e3158da0f7d4fe7f796c
Nate Schultz <nate.schultz@outlook.com>
2022-12-11 14:34:23 -0500

    added firebase social login handlers

--- src/pages/authentication/auth-forms/AuthLogin.js
+++ src/pages/authentication/auth-forms/AuthLogin.js
@@ -47,8 +47,8 @@ const AuthLogin = () => {
         <>
             <Formik
                 initialValues={{
-                    email: 'info@codedthemes.com',
-                    password: '123456',
+                    email: '',
+                    password: '',
                     submit: null
                 }}
                 validationSchema={Yup.object().shape({
--- src/pages/authentication/auth-forms/FirebaseSocial.js
+++ src/pages/authentication/auth-forms/FirebaseSocial.js
@@ -7,23 +7,59 @@ import Google from 'assets/images/icons/google.svg';
 import Twitter from 'assets/images/icons/twitter.svg';
 import Github from 'assets/images/icons/github.svg';
 
+// firebase
+import {
+    getAuth,
+    GoogleAuthProvider,
+    TwitterAuthProvider,
+    GithubAuthProvider
+} from "firebase/auth";
+
+
 // ==============================|| FIREBASE - SOCIAL BUTTON ||============================== //
 
 const FirebaseSocial = () => {
     const theme = useTheme();
     const matchDownSM = useMediaQuery(theme.breakpoints.down('sm'));
+    const provider = new GoogleAuthProvider();
+    const auth = getAuth();
 
     const googleHandler = async () => {
-        // login || singup
+        signInWithRedirect(auth, GoogleAuthProvider);
     };
 
     const twitterHandler = async () => {
-        // login || singup
+        signInWithRedirect(auth, TwitterAuthProvider);
     };
 
-    const facebookHandler = async () => {
-        // login || singup
+    const githubHandler = async () => {
+        signInWithRedirect(auth, GithubAuthProvider);
     };
+
+    const authCallbackSuccess = async (result) => {
+        // This gives you a Google Access Token. You can use it to access Google APIs.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+
+        // The signed-in user info.
+        const user = result.user;
+    }
+
+    const authCallbackFailure = async (error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+    }
+
+    useEffect(() => {
+        auth.useDeviceLanguage();
+        auth.getRedirectResult().then(authCallbackSuccess).catch(authCallbackFailure);
+    }, []);
 
     return (
         <Stack
@@ -55,7 +91,7 @@ const FirebaseSocial = () => {
                 color="secondary"
                 fullWidth={!matchDownSM}
                 startIcon={<img src={Github} alt="Github" />}
-                onClick={facebookHandler}
+                onClick={githubHandler}
             >
                 {!matchDownSM && 'Github'}
             </Button>
