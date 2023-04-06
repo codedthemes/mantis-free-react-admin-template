0218d50cc53ca4a14d1052ca6c663d4db116cd99
Abdelilah Jabri <42073961+JabriAbdelilah@users.noreply.github.com>
2023-04-06 18:20:22 +0000

    Fix: Warning: Failed prop type: Invalid prop `shadow` of type `function` supplied to `ForwardRef`, expected `string`

--- src/pages/authentication/AuthCard.js
+++ src/pages/authentication/AuthCard.js
@@ -2,14 +2,17 @@ import PropTypes from 'prop-types';
 
 // material-ui
 import { Box } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
 
 // project import
 import MainCard from 'components/MainCard';
 
 // ==============================|| AUTHENTICATION - CARD WRAPPER ||============================== //
 
-const AuthCard = ({ children, ...other }) => (
-    <MainCard
+const AuthCard = ({ children, ...other }) => {
+    const theme = useTheme();
+    
+    return (<MainCard
         sx={{
             maxWidth: { xs: 400, lg: 475 },
             margin: { xs: 2.5, md: 3 },
@@ -22,11 +25,11 @@ const AuthCard = ({ children, ...other }) => (
         {...other}
         border={false}
         boxShadow
-        shadow={(theme) => theme.customShadows.z1}
+        shadow={theme.customShadows.z1}
     >
         <Box sx={{ p: { xs: 2, sm: 3, md: 4, xl: 5 } }}>{children}</Box>
-    </MainCard>
-);
+    </MainCard>);
+}
 
 AuthCard.propTypes = {
     children: PropTypes.node
