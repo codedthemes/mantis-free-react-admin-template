c7961c7ae2fd245b9e27d0d498a6c75790cc8f63
Nate Schultz <nate.schultz@outlook.com>
2023-06-27 10:36:17 -0400

    fixed signin redirect bug

--- src/routes/PrivateRoutes.js
+++ src/routes/PrivateRoutes.js
@@ -2,8 +2,13 @@ import { Navigate } from 'react-router-dom';
 import { useAuth } from 'pages/authentication/auth-forms/AuthProvider';
 
 export default function PrivateRoute({ children }) {
-    const { currentUser } = useAuth();
-    console.log(currentUser);
+    const { user, loading } = useAuth();
 
-    return currentUser ? children : <Navigate to="/login" replace />;
+    // Show a loading indicator while waiting for the user
+    if (loading) {
+        return <div>Loading...</div>; // TODO: Replace this with your preferred loading indicator
+    }
+
+    // Once loaded, either show the children or navigate to login
+    return user ? children : <Navigate to="/login" replace />;
 }
