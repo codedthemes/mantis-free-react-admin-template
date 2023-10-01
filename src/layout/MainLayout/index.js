b2e02e354a8d18dce7f47ed91c1b95e63963a894
Lucas Kristandt <lucas.kristandt@web.de>
2023-10-01 09:20:14 +0000

    Update 2 files

--- src/layout/MainLayout/index.js
+++ src/layout/MainLayout/index.js
@@ -46,66 +46,73 @@ const MainLayout = () => {
   return (
     <Box
       sx={{
-        background: `radial-gradient(circle at 2% 10%, ${theme.palette.common.white}, transparent 100%),radial-gradient(circle at 95% 20%, ${theme.palette.primary[200]}, transparent 100%),radial-gradient(circle at 25% 90%, ${theme.palette.grey[300]}, transparent 100%)`,
-        display: 'grid',
-        width: '100%',
-        padding: theme.shape.layoutDesignGutterOuter,
-        gridTemplateColumns: `auto 1fr`,
-        gridTemplateRows: `auto 1fr`,
-        gap: theme.shape.layoutDesignGutter
+        background: `radial-gradient(circle at 2% 10%, ${theme.palette.common.white}, transparent 100%),radial-gradient(circle at 95% 20%, ${theme.palette.primary[200]}, transparent 100%),radial-gradient(circle at 25% 90%, ${theme.palette.grey[300]}, transparent 100%)`
       }}
     >
-      <Box sx={{ gridColumnStart: '2', gridColumnEnd: '3', gridRowStart: '1', gridRowEnd: '1' }}>
-        <Box sx={{ posiiton: 'sticky' }}>
-          <Header open={open} handleDrawerToggle={handleDrawerToggle} />
-        </Box>
-      </Box>
       <Box
         sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          gridColumnStart: '1',
-          gridColumnEnd: '1',
-          gridRowStart: '1',
-          gridRowEnd: '3'
+          display: 'grid',
+          width: '100%',
+          maxWidth: '1920px',
+          margin: '0 auto',
+          padding: theme.shape.layoutDesignGutterOuter,
+          gridTemplateColumns: `auto 1fr`,
+          gridTemplateRows: `auto 1fr`,
+          gap: theme.shape.layoutDesignGutter
         }}
       >
+        <Box sx={{ gridColumnStart: '2', gridColumnEnd: '3', gridRowStart: '1', gridRowEnd: '1' }}>
+          <Box sx={{ posiiton: 'sticky' }}>
+            <Header open={open} handleDrawerToggle={handleDrawerToggle} />
+          </Box>
+        </Box>
         <Box
           sx={{
-            position: 'sticky',
-            top: theme.shape.layoutDesignGutterOuter,
-            left: '0',
-            height: {
-              xs: `calc(100vh - ${theme.shape.layoutDesignGutterOuter.xs} - ${theme.shape.layoutDesignGutterOuter.xs})`,
-              sm: `calc(100vh - ${theme.shape.layoutDesignGutterOuter.sm} - ${theme.shape.layoutDesignGutterOuter.sm})`,
-              md: `calc(100vh - ${theme.shape.layoutDesignGutterOuter.md} - ${theme.shape.layoutDesignGutterOuter.md})`,
-              lg: `calc(100vh - ${theme.shape.layoutDesignGutterOuter.lg} - ${theme.shape.layoutDesignGutterOuter.lg})`
-            }
+            display: 'flex',
+            flexDirection: 'column',
+            gridColumnStart: '1',
+            gridColumnEnd: '1',
+            gridRowStart: '1',
+            gridRowEnd: '3'
           }}
         >
-          <LayoutBox
+          <Box
             sx={{
-              height: '100%',
-              backgroundColor: theme.palette.primary.main
+              position: 'sticky',
+              top: theme.shape.layoutDesignGutterOuter,
+              left: '0',
+              height: {
+                xs: `calc(100vh - ${theme.shape.layoutDesignGutterOuter.xs} - ${theme.shape.layoutDesignGutterOuter.xs})`,
+                sm: `calc(100vh - ${theme.shape.layoutDesignGutterOuter.sm} - ${theme.shape.layoutDesignGutterOuter.sm})`,
+                md: `calc(100vh - ${theme.shape.layoutDesignGutterOuter.md} - ${theme.shape.layoutDesignGutterOuter.md})`,
+                lg: `calc(100vh - ${theme.shape.layoutDesignGutterOuter.lg} - ${theme.shape.layoutDesignGutterOuter.lg})`
+              }
             }}
           >
-            <Drawer open={open} handleDrawerToggle={handleDrawerToggle} />
-          </LayoutBox>
+            <LayoutBox
+              sx={{
+                height: '100%',
+                backgroundColor: theme.palette.primary.main
+              }}
+            >
+              <Drawer open={open} handleDrawerToggle={handleDrawerToggle} />
+            </LayoutBox>
+          </Box>
         </Box>
-      </Box>
-      <Box
-        component="main"
-        sx={{
-          width: '100%',
-          flexGrow: 1,
-          gridColumnStart: '2',
-          gridColumnEnd: '2',
-          gridRowStart: '2',
-          gridRowEnd: '3',
-          backgroundColor: 'transparent'
-        }}
-      >
-        <Outlet />
+        <Box
+          component="main"
+          sx={{
+            width: '100%',
+            flexGrow: 1,
+            gridColumnStart: '2',
+            gridColumnEnd: '2',
+            gridRowStart: '2',
+            gridRowEnd: '3',
+            backgroundColor: 'transparent'
+          }}
+        >
+          <Outlet />
+        </Box>
       </Box>
     </Box>
   );
