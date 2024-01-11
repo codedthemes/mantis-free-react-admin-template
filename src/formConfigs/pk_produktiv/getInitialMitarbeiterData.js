9deac41c5a4e163b6f4f4d3ec495c2d1850f3c7f
Lucas Kristandt <lucas.kristandt@pinuts.de>
2024-01-11 19:05:06 +0100

    update initial mitarbeiter data

--- src/formConfigs/pk_produktiv/getInitialMitarbeiterData.js
+++ src/formConfigs/pk_produktiv/getInitialMitarbeiterData.js
@@ -5,7 +5,9 @@ const getInitialMitarbeiterData = (values) => {
     userId: uuid(),
     N9: 100,
     E9: values.annahmen_H23 || undefined,
-    F9: (values.annahmen_G26 || 0) * (values.annahmen_G18 || 0)
+    F9: (values.annahmen_G26 || 0) * (values.annahmen_G18 || 0),
+    G9: (values.annahmen_G27 || 0) * (values.annahmen_G18 || 0),
+    I9: (values.annahmen_G28 || 0) * (values.annahmen_G18 || 0)
   };
 };
 
