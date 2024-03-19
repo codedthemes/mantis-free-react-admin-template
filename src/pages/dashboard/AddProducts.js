f0e62783854fcac2b36c341f051cb1e56d48e03c
Alexis Bouletsis <alexisbouletsis@gmail.com>
2024-03-19 11:55:31 +0200

    added category and date selection

--- src/pages/dashboard/AddProducts.js
+++ src/pages/dashboard/AddProducts.js
@@ -6,12 +6,18 @@ const AddProducts = () => {
 
   const [name ,setName] = useState(''); 
   const [cost ,setCost] = useState(''); 
+  const [quantity ,setQuantity] = useState(''); 
+  const [category ,setCategory] = useState(''); 
+  const [orderDate ,setOrderDate] = useState(''); 
+  
 
   return (
     <Box sx={{ width: '100%', ml: { xs: 0, md: 1 } }}>
       <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '10vh'}}>
         <h1>Add new product</h1>
       </div>
+
+      {/* ==============================|| Add name of the product ||============================== */}
       <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
         <h3>Add name of the product</h3>
       </div>
@@ -23,6 +29,8 @@ const AddProducts = () => {
           />
         </FormControl>
       </div>
+
+      {/* ==============================|| Add cost of the product ||============================== */}
       <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
         <h3>Add cost of the product</h3>
       </div>
@@ -34,11 +42,55 @@ const AddProducts = () => {
           />
         </FormControl>
       </div>
+
+      {/* ==============================|| Add quantity of the product ||============================== */}
+      <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
+        <h3>Add quantity of the product</h3>
+      </div>
+      <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
+        <FormControl sx={{ width: { xs: '100%', md: 250 }}}>
+          <OutlinedInput
+            type="text" value={quantity} onChange={(e) => setQuantity(e.target.value)}
+            placeholder="Quantity"
+          />
+        </FormControl>
+      </div>
+
+      {/* ==============================|| Select category of the product ||============================== */}
+      <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
+        <h3>Select category of the product</h3>
+      </div>
+      <div style={{display: 'flex',justifyContent:'center', alignItems:'center' }}>
+        <FormControl sx={{ width: { xs: '100%', md: 250 }}}>
+          <select name="category" id="categoryID" value={category} onChange={(e) => setCategory(e.target.value)}>  
+            <option value="Coffee">Coffee</option>  
+            <option value="Non coffee">Non coffee</option>  
+            <option value="Bakery">Bakery</option>
+            <option value="Desert">Desert</option>
+          </select> 
+        </FormControl>
+      </div>
+
+      {/* ==============================|| Select date of order for the product ||============================== */}
+      <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
+        <h3>Select date of order</h3>
+      </div>
+      <div style={{display: 'flex',justifyContent:'center', alignItems:'center' }}>
+        <FormControl sx={{ width: { xs: '100%', md: 250 }}}>
+          <input type="date" id="date" value={orderDate} onChange={(e) => setOrderDate(e.target.value)}></input>
+        </FormControl>
+      </div>
+
+
       <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '10vh'}}>
         <button>Add product</button>
       </div>
+      
       <p>{name}</p>
       <p>{cost}</p>
+      <p>{quantity}</p>
+      <p>{category}</p>
+      <p>{orderDate}</p>
     </Box>
   );
 }
