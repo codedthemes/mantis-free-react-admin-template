6d36b40e359a7f76f008d5b2dd26a6dcd372c65e
Shadab Alam <shadab19it@gmail.com>
2024-06-24 12:42:54 +0000

    update config

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,5 +1,4 @@
 // https://github.com/vitejs/vite/discussions/3448
-import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import jsconfigPaths from 'vite-jsconfig-paths';
@@ -10,34 +9,8 @@ export default defineConfig({
   plugins: [react(), jsconfigPaths()],
   // https://github.com/jpuri/react-draft-wysiwyg/issues/1317
   base: '/app', // accessing env variable is not possible here. So hard coding this.
-  define: {
-    global: 'window'
-  },
+
   build:{
     outDir:"./build"
-  },
-  resolve: {
-    alias: [
-      {
-        find: /^~(.+)/,
-        replacement: path.join(process.cwd(), 'node_modules/$1')
-      },
-      {
-        find: /^src(.+)/,
-        replacement: path.join(process.cwd(), 'src/$1')
-      }
-    ]
-  },
-  server: {
-    // this ensures that the browser opens upon server start
-    open: true,
-    // this sets a default port to 3000
-    port: 3000
-  },
-  preview: {
-    // this ensures that the browser opens upon preview start
-    open: true,
-    // this sets a default port to 3000
-    port: 3000
   }
 });
