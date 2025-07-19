4c09093cf95e2d24b6e2926cd01d739af83304a8
ct-vrushti <vrushti.pcoded@gmail.com>
2025-07-19 13:50:22 +0530

    free version update

--- src/components/logo/LogoIcon.jsx
+++ src/components/logo/LogoIcon.jsx
@@ -4,9 +4,7 @@ import { useTheme } from '@mui/material/styles';
 /**
  * if you want to use image instead of <svg> uncomment following.
  *
- * import logoIconDark from 'assets/images/logo-icon-dark.svg';
  * import logoIcon from 'assets/images/logo-icon.svg';
- * import { ThemeMode } from 'config';
  *
  */
 
@@ -19,7 +17,7 @@ export default function LogoIcon() {
     /**
      * if you want to use image instead of svg uncomment following, and comment out <svg> element.
      *
-     * <img src={theme.palette.mode === ThemeMode.DARK ? logoIconDark : logoIcon} alt="Mantis" width="100" />
+     * <img src={logoIcon} alt="Mantis" width="100" />
      *
      */
     <svg width="129" height="129" viewBox="0 0 129 129" fill="none" xmlns="http://www.w3.org/2000/svg">
--- src/components/logo/LogoMain.jsx
+++ src/components/logo/LogoMain.jsx
@@ -1,23 +1,23 @@
+import PropTypes from 'prop-types';
 // material-ui
 import { useTheme } from '@mui/material/styles';
 
 /**
  * if you want to use image instead of <svg> uncomment following.
  *
- * import logoDark from 'assets/images/logo-dark.svg';
  * import logo from 'assets/images/logo.svg';
  *
  */
 
 // ==============================|| LOGO SVG ||============================== //
 
-export default function LogoMain() {
+export default function LogoMain({ reverse }) {
   const theme = useTheme();
   return (
     /**
      * if you want to use image instead of svg uncomment following, and comment out <svg> element.
      *
-     * <img src={theme.palette.mode === ThemeMode.DARK ? logoDark : logo} alt="Mantis" width="100" />
+     * <img src={logo} alt="Mantis" width="100" />
      *
      */
     <>
@@ -57,3 +57,5 @@ export default function LogoMain() {
     </>
   );
 }
+
+LogoMain.propTypes = { reverse: PropTypes.bool };
